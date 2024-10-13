772742664e0680d6473e8bbb87aecc4adc00496e
Constantinos-Mavrommatis <constantinosmavrommatis2000@gmail.com>
2024-10-13 15:44:05 +0100

    Fixed all problems and added a user input

--- Lecture06_Practice/Exercises/bash_06.sh
+++ Lecture06_Practice/Exercises/bash_06.sh
@@ -5,25 +5,25 @@
 inputfile="/localdisk/home/s2748062/Exercises/Lecture06_Practice/Exercises/blastoutput2.out"
 
 echo -e "${inputfile}"
-unset IFS
+rm -f *.exercise.out
 
-while read wholeline
-do
-	# We need to get all the lines without the "#"
-	if [[ "${wholeline:0:1}" != "#" ]]
-	then
-		read Q_acc S_acc pc_identity alignment_length mismatches gap_opens Q_start Q_end S_start S_end evalue bitscore <<< ${wholeline}
-		echo -e "${S_acc}"
-		echo -e "${S_acc}" >> Subject_accessions.exercise.out
-	fi
+read -p  "Would you like a list of all the Subject Acessions of the Hpos (YES/NO): " run_S_acc
 
-
-
-
-	# We need to list the subject accession filed
-
-done < ${inputfile}
-
-
+if [[ "${run_S_acc}" == YES ]]
+then
+	while read wholeline
+	do
+		if [[ "${wholeline:0:1}" != "#" ]]  # We need to get all the lines without the "#"
+		then
+			read Q_acc S_acc pc_identity alignment_length mismatches gap_opens Q_start Q_end S_start S_end evalue bitscore <<< ${wholeline} #Here we seperating the fields 
+			echo -e "${S_acc}"
+			echo -e "${S_acc}" >> Subject_accessions.exercise.out #We list the subject access
+		fi
+	
+	done < ${inputfile}
+echo -e "DONE"
+else
+	echo "Skipping"
+fi
 	
 
