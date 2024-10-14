c261a771168505ef3278abd3d39c3a8633553f20
Constantinos-Mavrommatis <constantinosmavrommatis2000@gmail.com>
2024-10-15 00:38:48 +0100

    Everything works and added a basic sum of the pairs for each sample

--- Lecture07/My_First_Pipeline/Script.sh
+++ Lecture07/My_First_Pipeline/Script.sh
@@ -2,7 +2,7 @@
 
 
 input="$PWD/*.fq.gz"
-File_name=$(cat Tco2.fqfiles | awk 'BEGIN{FS="\t";}{print $1, $6, $7;}' | grep "Tco" | head -1) #
+File_name=$(cat Tco2.fqfiles | awk 'BEGIN{FS="\t";}{print $1, $6, $7;}' | grep "Tco" | head -2) #
 Output="$PWD/Outputs"
 rm -f /localdisk/home/s2748062/Exercises/Lecture07/My_First_Pipeline/Outputs/Tco* 
 
@@ -15,38 +15,43 @@ then
 		echo -e "Proccessing: ${File_Name} with ${Pair_1}\t${Pair_2}"
 		fastqc -o ${Output} -t 128 ${Pair_1} ${Pair_2}
 	done <<< ${File_name}
-else
-	echo "Skipping"
-fi
+	
+	read -p "Would you like to get a summary of the files ? (YES/NO): " script_run_2
+	
+	if [[ ${script_run_2} == YES ]]
+	then
+		while read File_Name Pair_1 Pair_2
+		do
+			Pair_1_Sum=${Pair_1%.fq.gz}
+			Pair_2_Sum=${Pair_2%.fq.gz}
+			
+			Summarize_1=$(unzip -p "${Output}/${Pair_1_Sum}_fastqc.zip" "*fastqc/fastqc_data.txt")
+			Summarize_2=$(unzip -p "${Output}/${Pair_2_Sum}_fastqc.zip" "*fastqc/fastqc_data.txt")
+
+			echo -e "Summarizing file: ${File_Name}"
+
+			filename_Pair_1=$(echo "${Summarize_1}" | grep "Filename" | cut -f 2)
+			total_sequences_Pair_1=$(echo "${Summarize_1}" | grep "Total Sequences" | cut -f 2)
+			sequence_length_Pair_1=$(echo "${Summarize_1}" | grep "Sequence length" | cut -f 2)
+			gc_content_Pair_1=$(echo "${Summarize_1}" | grep "%GC" | awk 'BEGIN{FS="\t";}{print $2}') 
+			
+			filename_Pair_2=$(echo "${Summarize_2}" | grep "Filename" | cut -f 2)
+			total_sequences_Pair_2=$(echo "${Summarize_2}" | grep "Total Sequences" | cut -f 2)
+			sequence_length_Pair_2=$(echo "${Summarize_2}" | grep "Sequence length" | cut -f 2)
+			gc_content_Pair_2=$(echo "${Summarize_2}" | grep "%GC")
+			
+			echo -e "Total Sequences for ${filename_Pair_1}: ${total_sequences_Pair_1}"
+			echo -e "Total Sequence Length for ${filename_Pair_1}: ${sequence_length_Pair_1}"
+			echo -e "Total GC content for ${filename_Pair_1}: ${gc_content_Pair_1}"
+			
+			echo "Total Sequences for Pair 2: ${total_sequences_Pair_2}"
 
 
-read -p "Would you like to get a summary of the files ? (YES/NO): " script_run_2
-
-if [[ ${script_run_2} == YES ]]
-then
-	while read File_Name Pair_1 Pair_2
-	do
-		Pair_1_Sum=${Pair_1%.fq.gz}
-		Pair_2_Sum=${Pair_2%.fq.gz}
-		Summarize=$(unzip -p "${Output}/${Pair_1_Sum}_fastqc.zip" "*fastqc/fastqc_data.txt")
 		
-		echo -e "Summarizing file: ${File_Name}"
-		
-		filename_Pair_1=$(echo "${Summarize}" | grep "Filename" | cut -f 2)
-		total_sequences_Pair_1=$(echo "${Summarize}" | grep "Total Sequences" | cut -f 2)
-		sequence_length_Pair_1=$(echo "${Summarize}" | grep "Sequence length" | cut -f 2)
-		gc_content_Pair_1=$(echo "${Summarize}" | grep "%GC" | cut -f 2)
-
-		filename_Pair_2=$(echo "${Summarize}" | grep "Filename" | cut -f 2)
-                total_sequences_Pair_2=$(echo "${Summarize}" | grep "Total Sequences" | cut -f 2)
-                sequence_length_Pair_2=$(echo "${Summarize}" | grep "Sequence length" | cut -f 2)
-                gc_content_Pair_2=$(echo "${Summarize}" | grep "%GC" | cut -f 2)
-
-
-		echo "Total Sequences for Pair 1: ${total_sequences_Pair_1}"
-		echo "Total Sequences for Pair 2: ${total_sequences_Pair_2}"
-
-	done <<< ${File_name}
+		done <<< ${File_name}
+	else
+		echo "Skipping"
+	fi
 else
 	echo "Skipping"
 fi
