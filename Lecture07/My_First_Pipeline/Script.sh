7c9c552bc2c235fd370b7f296ee1509206af76e0
Constantinos-Mavrommatis <constantinosmavrommatis2000@gmail.com>
2024-10-14 13:42:24 +0100

    Improved the script does analysis on all files

--- Lecture07/My_First_Pipeline/Script.sh
+++ Lecture07/My_First_Pipeline/Script.sh
@@ -1,10 +1,19 @@
 #!/usr/bin/bash
 
-File_name=$(cat Tco2.fqfiles | awk 'BEGIN{FS="\t";}{print $1;}' | grep "Tco")
 
-echo -e "${File_name}"
+input="$PWD/*.fq.gz"
+
+File_name=$(cat Tco2.fqfiles | awk 'BEGIN{FS="\t";}{print $1, $6, $7;}' | grep "Tco") 
+Output="$PWD/Outputs"
+rm -f /localdisk/home/s2748062/Exercises/Lecture07/My_First_Pipeline/Outputs/Tco* 
+
+while read File_Name Pair_1 Pair_2
+do
+	echo -e "Proccessing: ${File_Name} with ${Pair_1}\t${Pair_2}"
+	fastqc -o ${Output} -t 128 ${Pair_1} ${Pair_2}
 
 
+done <<< ${File_name}
 
 
 
